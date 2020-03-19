c726dd6b06f1d399dff3816f05107c0bf1c56bde
Tj-Palm <48101800+Tj-Palm@users.noreply.github.com>
2020-03-19 11:37:05 +0100

    HoverDarkTable

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,36 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+import Axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index";
+
+import {json2table100} from "./genericTable";
+
+let BaseUri: string = "http://anbo-bookstorerest.azurewebsites.net/api/"
+let contentElement: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
+let element: HTMLTableElement = <HTMLTableElement> document.getElementById("table_content");
+let AllBooks: JSON;
+
+interface IBook {
+   id: number,
+   title: string,
+   author: string,
+   publisher: string,
+   price: number,
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
-
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+ Axios.get(BaseUri + "Books").then(
+     function(response: AxiosResponse): void{
+         console.log("getting books... v4");
+         setTimeout(() => {  console.log("waited 5 sek"); }, 5000);
+    let data: IBook[] = response.data;
+        console.log(data);
+    let result: string = json2table100(data);
+console.log(result);
+        element.innerHTML = result;
+     }
+ )
+ .catch(
+     function(error: AxiosError): void{
+        contentElement.innerHTML = error.message;
+     }
+ )
